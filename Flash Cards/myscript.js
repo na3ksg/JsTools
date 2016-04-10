54c85590e1d28331b9404dcb715d629307f1be07
na3ksg <superkasago@yahoo.co.jp>
2016-04-10 23:14:24 +0900

    単語帳完成

---
+++ Flash Cards/myscript.js
@@ -0,0 +1,53 @@
+(function() {
+    'use strict';
+
+    var words = [
+      {'en': 'fly', 'ja': '飛ぶ' },
+      {'en': 'push', 'ja': '押す' },
+      {'en': 'pull', 'ja': '引く' },
+      {'en': 'run', 'ja': '走る' },
+      {'en': 'walk', 'ja': '歩く' }
+    ];
+
+    var card = document.getElementById('card');
+    var cardFront = document.getElementById('card-front');
+    var cardBack = document.getElementById('card-back');
+    var btn = document.getElementById('btn');
+    card.addEventListener('click', function() {
+      flip();
+    });
+    btn.addEventListener('click', function() {
+      next();
+    });
+    
+    function next() {
+        if(card.className === 'open') {
+            card.addEventListener('transitionend', setCard);
+            flip();
+        } else {
+            setCard();
+        }
+        
+    }
+    function setCard() {
+        var num = Math.floor(Math.random() * words.length);
+        cardFront.innerHTML = words[num]['en'];
+        cardBack.innerHTML = words[num]['ja'];
+        card.removeEventListener('transitionend', setCard);
+    }
+    
+    setCard();
+    
+    window.addEventListener('keyup', function(e) {
+        if(e.keyCode === 70) {
+            flip();
+        } else if(e.keyCode === 78) {
+            next();
+        }
+    });
+
+    function flip() {
+      card.className = card.className === '' ? 'open' : '';
+    }
+
+  })();
