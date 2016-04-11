ed98178201698958742e9daad6fc83e492642ae3
na3ksg <superkasago@yahoo.co.jp>
2016-04-11 11:14:20 +0900

    コード整形

--- Flash Cards/myscript.js
+++ Flash Cards/myscript.js
@@ -2,11 +2,11 @@
     'use strict';
 
     var words = [
-      {'en': 'fly', 'ja': '飛ぶ' },
-      {'en': 'push', 'ja': '押す' },
-      {'en': 'pull', 'ja': '引く' },
-      {'en': 'run', 'ja': '走る' },
-      {'en': 'walk', 'ja': '歩く' }
+        { 'en': 'fly', 'ja': '飛ぶ' },
+        { 'en': 'push', 'ja': '押す' },
+        { 'en': 'pull', 'ja': '引く' },
+        { 'en': 'run', 'ja': '走る' },
+        { 'en': 'walk', 'ja': '歩く' }
     ];
 
     var card = document.getElementById('card');
@@ -14,20 +14,20 @@
     var cardBack = document.getElementById('card-back');
     var btn = document.getElementById('btn');
     card.addEventListener('click', function() {
-      flip();
+        flip();
     });
     btn.addEventListener('click', function() {
-      next();
+        next();
     });
-    
+
     function next() {
-        if(card.className === 'open') {
+        if (card.className === 'open') {
             card.addEventListener('transitionend', setCard);
             flip();
         } else {
             setCard();
         }
-        
+
     }
     function setCard() {
         var num = Math.floor(Math.random() * words.length);
@@ -35,19 +35,19 @@
         cardBack.innerHTML = words[num]['ja'];
         card.removeEventListener('transitionend', setCard);
     }
-    
+
     setCard();
-    
+
     window.addEventListener('keyup', function(e) {
-        if(e.keyCode === 70) {
+        if (e.keyCode === 70) {
             flip();
-        } else if(e.keyCode === 78) {
+        } else if (e.keyCode === 78) {
             next();
         }
     });
 
     function flip() {
-      card.className = card.className === '' ? 'open' : '';
+        card.className = card.className === '' ? 'open' : '';
     }
 
-  })();
+})();
